5e4eafc5434c60fb4a821ecfe93133a93371d623
juanpditella <jditella@alumnos.exa.unicen.edu.ar>
2023-08-28 17:08:55 -0300

    Gateway finalizado.

---
+++ src/main/java/GatewayAlboChallenge/GatewayAlboChallengeApplication.java
@@ -0,0 +1,13 @@
+package GatewayAlboChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayAlboChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayAlboChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GatewayAlboChallenge/GatewayAlboChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package GatewayAlboChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayAlboChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
